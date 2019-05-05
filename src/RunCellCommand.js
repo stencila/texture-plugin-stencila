302bd23cd8f2b932e0f392893f38a25d0807280a
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-05-05 21:52:22 +0200

    Implement RunCellCommand.

--- src/RunCellCommand.js
+++ src/RunCellCommand.js
@@ -1,11 +1,37 @@
 import { Command } from 'substance'
+import StencilaCell from './StencilaCell'
+import StencilaInlineCell from './StencilaInlineCell'
+import JavascriptContextService from './jscontext/JavascriptContextService'
 
 export default class RunCellCommand extends Command {
   static get id () { return 'stencila:run-cell' }
 
-  getCommandState () {
+  getCommandState (params, context) {
+    const selectionState = params.selectionState
+    const node = selectionState.node
+    if (node && (node.type === StencilaCell.type || node.type === StencilaInlineCell.type)) {
+      return {
+        disabled: false,
+        nodeId: node.id
+      }
+    }
     return { disabled: true }
   }
 
-  execute () {}
+  execute (params, context) {
+    let editorSession = params.editorSession
+    let commandState = params.commandState
+    // TODO: the service should be language specific
+    context.config.getService(JavascriptContextService.id).then(service => {
+      let cell = editorSession.getDocument().get(commandState.nodeId)
+      service.requestExecution(cell.id, cell.source, (err, res) => {
+        if (err) {
+          // TODO: define the interface properly
+          editorSession.updateNodeStates([[cell.id, { value: undefined, errors: err.errors }]], { propagate: true })
+        } else {
+          editorSession.updateNodeStates([[cell.id, { value: res.value, errors: null }]], { propagate: true })
+        }
+      })
+    })
+  }
 }
--- src/StencilaCellComponent.js
+++ src/StencilaCellComponent.js
@@ -1,18 +1,61 @@
 import { Component } from 'substance'
 
 export default class StencillaCellComponent extends Component {
+  didMount () {
+    this.context.appState.addObserver(['document'], this._onNodeUpdate, this, {
+      stage: 'render',
+      document: { path: [this.props.node.id] }
+    })
+  }
+
+  dispose () {
+    this.context.appState.removeObserver(this)
+  }
+
   render ($$) {
     const node = this.props.node
+    const nodeState = node.state
     const CodeEditor = this.getComponent('code-editor')
-    return $$('span').addClass('sc-stencila-cell').append(
-      $$(CodeEditor, {
-        document: node.getDocument(),
-        path: [node.id, 'source'],
-        // TODO: the language should come from somewhere else
-        // in a classical Notebook, this would be set document wide
-        // in a polyglot environment this would come from the cell
-        language: 'javascript'
-      })
-    )
+    let el = $$('div').addClass('sc-stencila-cell')
+    let editor = $$(CodeEditor, {
+      document: node.getDocument(),
+      path: [node.id, 'source'],
+      // TODO: the language should come from somewhere else
+      // in a classical Notebook, this would be set document wide
+      // in a polyglot environment this would come from the cell
+      language: 'javascript'
+    }).ref('editor')
+    el.append(editor)
+
+    if (nodeState) {
+      el.append(this._renderErrors($$, nodeState))
+      el.append(this._renderValue($$, nodeState))
+    }
+    return el
+  }
+
+  _renderErrors ($$, nodeState) {
+    if (nodeState.errors && nodeState.errors.length > 0) {
+      let errorsEl = $$('div').addClass('se-errors')
+      errorsEl.append(
+        nodeState.errors.map(err => {
+          // TODO: we have to specify a common format for errors
+          return $$('div').addClass('se-error').text(err.description)
+        })
+      )
+      return errorsEl
+    }
+  }
+
+  _renderValue ($$, nodeState) {
+    if (nodeState.value) {
+      return $$('div').addClass('se-value').text(nodeState.value)
+    }
+  }
+
+  _onNodeUpdate (change) {
+    if (change.info.action === 'node-state-update') {
+      this.rerender()
+    }
   }
 }
